fc7c11eab92c0ac9e1e89d050c12380d2747b705
Vinay Kumar <vinaykumar.dr@rib-software.com>
2026-03-30 16:52:27 +0000

    build configuration fix

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,7 @@
 import { initFederation } from '@angular-architects/native-federation';
+import { environment } from './environments/environment';
 
-initFederation('/assets/federation.manifest.json')
+initFederation(environment.federationManifest)
   .catch((err) => console.error(err))
   .then((_) => import('./bootstrap'))
   .catch((err) => console.error(err));
